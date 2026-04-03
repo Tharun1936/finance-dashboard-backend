0ecdb5c6b93d4e3108e5eb8142241af061aaf682
Tharun1936 <tharunmgowda1936@gmail.com>
2026-04-03 14:45:42 +0530

    finalll

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // This is the main entry point of our backend server
 // It connects to MongoDB first, then starts the Express server
 
-// Load environment variables from .env file
+// Load environment variables from .env file !!
 require('dotenv').config();
 
 const connectDB = require('./src/config/db');
@@ -9,7 +9,7 @@ const app = require('./src/app');
 
 const PORT = process.env.PORT || 5000;
 
-// First connect to the database, then start the server
+// First connect to the database, then start the server!!
 // We don't want to accept requests before the DB is ready
 connectDB().then(() => {
   app.listen(PORT, () => {
